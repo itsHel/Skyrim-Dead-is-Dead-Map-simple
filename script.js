038c1cebfb9ae906a7024facd76278937b06687c
itsHel <51739304+itsHel@users.noreply.github.com>
2022-03-09 12:54:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,325 @@
+// Colors - AA1111 FFFFFF EBEBEB 000000
+
+(function(){
+    const $ = document.querySelector.bind(document);
+    const $$ = document.querySelectorAll.bind(document);
+
+    const map = $("#map");
+    const mapWrapper = $("#map-wrapper");
+    const baseImg = "5_2016.png";
+    const counterSize = 32;
+
+    var mapScale = parseFloat(localStorage["scale"] || 1);
+    // Object of arrays of times when was death counted, arrays are named as coords: "x142y444"
+    var deaths = JSON.parse(localStorage["data"] || "{}");
+    var mapType = localStorage["mapType"] || baseImg;
+    map.src = "img/" + mapType;
+
+    window.addEventListener("load", function(){
+        init();
+    });
+    
+    if(map.complete){
+        mapWrapper.style.opacity = 1;
+        mapWrapper.style.transform = "none";
+    } else {
+        map.addEventListener("load", function(){
+            mapWrapper.style.opacity = 1;
+            mapWrapper.style.transform = "none";         
+        });                
+    }
+
+    function init(){
+        document.documentElement.style.setProperty("--counter-size", counterSize + "px");
+
+        $("nav").style.width = "calc(100vw - " + (window.innerWidth - document.documentElement.clientWidth) + "px)";
+
+        // Select
+        let typeSelect = $("#type-select");
+
+        if(mapType != baseImg){
+            typeSelect.value = mapType;
+        }
+
+        typeSelect.addEventListener("change", function(){
+            map.src = "img/" + this.value;
+
+            window.localStorage["mapType"] = this.value;
+        });
+
+        customSelect(typeSelect, {theme: "dark"});
+
+        // Load history
+        let i = 0;
+        let listHtml = "";
+        for(const prop in deaths){
+            let x = prop.match(/x(\d+)y/)[1];
+            let y = prop.match(/y(\d+)s/)[1];
+            let scale = prop.match(/s(.+)/)[1];
+
+            insertCounter(x, y, scale, deaths[prop].length);
+            listHtml += getListItemHtml("x" + x + "y" + y + "s" + scale, deaths[prop].length);
+            i += deaths[prop].length;
+        }
+
+        $("#count-val").textContent = i;
+        if(i){
+            $("#sidemenu-list").insertAdjacentHTML("beforeend", listHtml);
+            $("#sidemenu-wrapper").style.display = "flex";
+        }
+
+        if(mapScale != 1){
+            if(map.complete){
+                setZoom();
+            } else {
+                map.addEventListener("load", function(){
+                    setZoom();
+                });
+            }
+        }
+
+        $(".plus").addEventListener("click", function(){
+            let newScale = mapScale;
+        
+            newScale = (newScale * 1.1).toFixed(2);
+            newScale = Math.ceil(newScale * 10) / 10;
+            newScale = Math.min(8, newScale);
+        
+            mapScale = newScale;
+            setZoom();
+        });
+        $(".minus").addEventListener("click", function(){
+            let newScale = mapScale;
+
+            newScale = (newScale / 1.1).toFixed(2);
+            newScale = Math.floor(newScale * 10) / 10;
+            newScale = Math.max(0.2, newScale);
+
+            mapScale = newScale;
+            setZoom();
+        });
+
+        $("#sidemenu-toggle").addEventListener("click", function(){
+            let wrapper = this.parentNode;
+            if(wrapper.classList.contains("side-hidden")){
+                // Show
+                wrapper.classList.remove("side-hidden");
+            } else {
+                // Hide
+                wrapper.classList.add("side-hidden");
+            }
+        });
+
+        $("#sidemenu").addEventListener("mouseup", function(e){
+            if(e.target.nodeName == "LI"){
+                let target = document.getElementById("counter-" + e.target.id);             // Must be getElementById because id contains dot
+
+                target.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center',
+                    inline: 'center'
+                });
+
+                target.focus({preventScroll: true});
+            }
+        });
+
+        mapWrapper.addEventListener("click", function(e){
+            if(e.target.nodeName != "IMG")
+                return;
+
+                let y = e.offsetY;
+                let x = e.offsetX;
+
+                createCounter(x, y, mapScale);
+        });
+
+        window.addEventListener("wheel", wheelZoom, {passive: false});
+
+        window.addEventListener("beforeunload", function(){
+            localStorage["scale"] = mapScale;
+        });
+    }
+
+    function wheelZoom(e){
+        if(e.ctrlKey){
+            e.preventDefault();
+            if(e.deltaY < 0){
+                $(".plus").click();
+            } else {
+                $(".minus").click();
+            }
+        }
+    }
+
+    // Only on page load
+    function insertCounter(x, y, scale, count){
+        let saveX = x * (mapScale / scale);
+        let saveY = y * (mapScale / scale);
+
+        let html = getCounterHtml(count, saveX, saveY, x, y, scale);
+
+        mapWrapper.insertAdjacentHTML("beforeend", html);
+
+        let div = $(".counter:last-of-type");
+        let countDiv = div.querySelector(".count");
+
+        div.addEventListener("click", function(e){
+            let remove = false;
+
+            if(!e.target.closest(".arrow-bottom")){
+                // ++
+                countDiv.textContent = parseInt(countDiv.textContent) + 1;
+                
+                $("#count-val").textContent = parseInt($("#count-val").textContent) + 1;
+            } else {
+                // --
+                remove = true;
+                countDiv.textContent = parseInt(countDiv.textContent) - 1;
+
+                $("#count-val").textContent = parseInt($("#count-val").textContent) -1;
+
+                if(countDiv.textContent == 0){
+                    countDiv.closest(".counter").remove();
+                }
+            }
+
+            save(x, y, scale, remove);
+        });
+    }
+
+    // On map click
+    function createCounter(x, y, scale){
+        let saveX = x - ((counterSize / 2) * scale);
+        let saveY = y - ((counterSize / 2) * scale);
+        
+        let html = getCounterHtml(1, saveX, saveY);
+
+        mapWrapper.insertAdjacentHTML("beforeend", html);
+
+        let div = $(".counter:last-of-type");
+        let countDiv = div.querySelector(".count");
+        div.focus();
+
+        $("#count-val").textContent = parseInt($("#count-val").textContent) + 1;
+        save(saveX, saveY, scale);
+
+        div.addEventListener("click", function(e){
+            let remove = false;
+
+            if(!e.target.closest(".arrow-bottom")){
+                // ++
+                countDiv.textContent = parseInt(countDiv.textContent) + 1;
+                
+                $("#count-val").textContent = parseInt($("#count-val").textContent) + 1;
+            } else {
+                // --
+                remove = true;
+                countDiv.textContent = parseInt(countDiv.textContent) - 1;
+
+                $("#count-val").textContent = parseInt($("#count-val").textContent) -1;
+
+                if(countDiv.textContent == 0){
+                    countDiv.closest(".counter").remove();
+                }
+            }
+
+            save(saveX, saveY, scale, remove);
+        });
+    }
+
+    function getCounterHtml(count, x, y, basex = x, basey = y, scale = mapScale){
+        let html = `<div id="counter-x${basex}y${basey}s${scale}" class="counter" style="left: ${x}px; top: ${y}px; transform:scale(${mapScale})" data-basex="${basex}" data-basey="${basey}" data-scale=${scale} tabindex=${count}>
+                        <span class="arrow arrow-top"><svg viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z"/></svg></span>
+                        <span class=count>${count}</span>
+                        <span class="arrow arrow-bottom"><svg viewBox="0 0 24 24"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg></span>
+                    </div>`;
+        return html;
+    }
+
+    function setZoom(){
+        // Save scroll
+        let oldWidth = $("body").offsetWidth;
+        let widthRatio = window.scrollX / oldWidth;
+        let oldHeight = $("body").offsetHeight;
+        let heightRatio = window.scrollY / oldHeight;
+
+        map.width = map.dataset.width * mapScale;
+        map.height = map.dataset.height * mapScale;
+
+        // Zoom texts
+        $$(".counter").forEach((el) => {
+            let zoomEdit = mapScale / el.dataset.scale;
+            el.style.left = (parseInt(el.dataset.basex) * zoomEdit) + "px";
+            el.style.top = (parseInt(el.dataset.basey) * zoomEdit) + "px";
+            el.style.transform = "scale(" + mapScale + ")";
+        });
+
+        $("#scale-val").textContent = parseInt(mapScale * 100);
+
+        // Scroll to previous pos
+        let newHeight = $("body").offsetHeight;
+        let newTop = newHeight * heightRatio;
+        
+        let newWidth = $("body").offsetWidth;
+        let newLeft = newWidth * widthRatio;
+
+        window.scroll({left: newLeft, top: newTop});
+    }
+
+    function save(x, y, scale, remove = false){
+        let divName = "x" + parseInt(x) + "y" + parseInt(y) + "s" + scale;
+        let newAdded = false;
+
+        if(remove){
+            deaths[divName].pop();
+
+            if(!deaths[divName].length){
+                delete deaths[divName];
+            }
+        } else {
+            if(!(deaths[divName])){
+                deaths[divName] = [];
+
+                $("#sidemenu-list").insertAdjacentHTML("beforeend", getListItemHtml(divName));
+                newAdded = true;
+            }
+
+            deaths[divName].push(Math.round(Date.now()/1000));
+        }
+
+        if(!newAdded){
+            editList(divName, remove);
+        }
+
+        let total = $("#count-val").textContent;
+        if(total == 0){
+            $("#sidemenu-wrapper").style.display = "none";
+        } else if(total == 1){
+            $("#sidemenu-wrapper").style.display = "flex";
+        }
+
+        localStorage["data"] = JSON.stringify(deaths);
+    }
+
+    function editList(id, remove){
+        let target = document.getElementById(id);           // Must be getElementById because id contains dot
+        let content = target.textContent;
+
+        if(remove){
+            target.textContent = parseInt(content) - 1;
+            
+            if(target.textContent == 0){
+                target.remove();
+            }
+        } else {
+            target.textContent = parseInt(content) + 1;
+        }
+    }
+
+    function getListItemHtml(id, count = 1){
+        let html = `<li id="${id}">${count}</li>`;
+
+        return html;
+    }
+})();
---
+++ uti/select.js
@@ -0,0 +1,211 @@
+'use strict';
+
+// Themes: "light", "dark", "split"
+// Replaces already existing select
+// Accepts element or nodeList
+function customSelect(elements, optionsPicked = {}){
+    // Todo:    - 
+    
+    // Set base select display:none; in css
+    // Alternative chevron: ›   ˆ   ∟   ❯
+    // Use "vertical-align: top;" to stop vertical center
+
+    // Set defaults
+    let options = {
+        maxHeight: optionsPicked.maxHeight ?? 400,
+        maxWidth: optionsPicked.maxWidth ?? 600,
+        scrollOnKey: optionsPicked.scrollOnKey ?? false,
+        theme: optionsPicked.theme ?? "",                       // Themes: "light", "dark", "split"
+        // Callback function which stops select from being slided up if clicked on item (allows to click on multiple items without closing)
+        // dontCloseCallback must return true, returning flse resets select
+        dontCloseCallback: optionsPicked.dontCloseCallback ?? false,
+        // Boolean, otherwise same as above
+        dontClose: optionsPicked.dontClose ?? false
+    }
+
+    let originalSelectChangedByThis = false;
+
+    if(options.theme){
+        let turnOn;
+        if(options.theme == "split"){
+            turnOn = "light";
+        } else {
+            turnOn = options.theme;
+        }
+
+        document.documentElement.style.setProperty("--select-light", "var(--OFF)");
+        document.documentElement.style.setProperty("--select-" + turnOn, "var(--ON)");
+    }
+    
+    if(elements instanceof Element){
+        elements = [elements];
+    }
+
+    elements.forEach(function(element){
+        let selectOriginal = element;
+        selectOriginal.style.display = "none";
+
+        if(selectOriginal.nextElementSibling?.classList.contains("custom-select-wrapper")){
+            selectOriginal.nextElementSibling.remove();
+        }
+
+        let headerSelect;
+        // let selectFull = "<div class=\"custom-select-wrapper " + (selectOriginal.getAttribute("class") ?? "") + "\">";
+        let selectFull = "<div class=\"custom-select-wrapper " + ((options.theme == "split") ? "split" : "") + "\">";
+        let items = "<div class=\"select-items-wrapper\">";
+
+        selectOriginal.querySelectorAll("option").forEach(function(option){
+            let selectClass = "";
+            if(option.getAttribute("selected") == "selected" || option.getAttribute("selected")){
+                headerSelect = "<div class=\"custom-select-header\"><span>" + option.textContent + "</span><span class=\"chevron\"></span></div>";
+                selectClass = "custom-selected";
+            }
+
+            items += "<div title=\"" + option.textContent + "\" class=\"custom-select-item " + selectClass + "\">" + option.textContent + "</div>";
+        });
+
+        if(!headerSelect){
+            let text = selectOriginal.querySelector("option").textContent;
+            headerSelect = "<div class=\"custom-select-header\"><span>" + text + "</span><span class=\"chevron\"></span></div>";
+            items = items.replace("custom-select-item ", "custom-select-item custom-selected");
+        }
+
+        selectFull += headerSelect + items + "</div></div>";
+        selectOriginal.insertAdjacentHTML("afterend", selectFull);
+
+        let selectFullWrapper = selectOriginal.nextElementSibling;
+        let selectWrapper = selectFullWrapper.querySelector(".select-items-wrapper");
+        let selectHeader = selectFullWrapper.querySelector(".custom-select-header");
+        let height = selectWrapper.offsetHeight;
+        
+        // Setup minWidth of header text same as width of longest item
+        let width = (parseFloat(selectWrapper.offsetWidth) - 2 * parseFloat(getComputedStyle(selectWrapper.querySelector("div")).padding)).toString() + "px";
+        selectFullWrapper.querySelector("span:first-of-type").style.width = width;
+
+        selectWrapper.style.maxHeight = "0px";
+        selectWrapper.style.maxWidth = options.maxWidth + "px";
+        selectFullWrapper.style.maxWidth = options.maxWidth + "px";
+
+        selectWrapper.querySelectorAll(".custom-select-item").forEach(function(item, index){
+            item.addEventListener("click", function(){
+                if(options.dontClose || (options.dontCloseCallback && options.dontCloseCallback())){
+                    if(item.classList.contains("custom-selected-multi")){
+                        item.classList.remove("custom-selected-multi");
+                    } else {
+                        item.classList.add("custom-selected-multi");
+                    }
+                } else {
+                    [...item.parentNode.children].forEach(el => el.classList.remove("custom-selected"));
+                    item.classList.add("custom-selected");
+
+                    originalSelectChangedByThis = true;
+                    selectOriginal.nextElementSibling.querySelector("span").textContent = this.textContent;
+
+                    [...selectOriginal.children].forEach(function(el, _index){
+                        if(index == _index){
+                            el.setAttribute("selected", true);
+                            selectOriginal.selectedIndex = index;
+                        } else {
+                            el.removeAttribute("selected");
+                        }
+                    });
+
+                    let changeE = new Event("change");
+                    selectOriginal.dispatchEvent(changeE);
+                }
+            });
+        });
+
+        selectHeader.addEventListener("click", function(){
+            if(!selectWrapper.innerHTML)
+                return;
+
+            if(options.dontClose || (options.dontCloseCallback && options.dontCloseCallback())){
+                selectWrapper.querySelector(".custom-selected")?.classList.remove("custom-selected");
+            } else {
+                selectWrapper.querySelectorAll(".custom-selected-multi").forEach(item => item.classList.remove("custom-selected-multi"));
+            }
+
+            // Header
+            if(selectWrapper.style.maxHeight == "0px"){
+                if(height == 0){
+                    selectWrapper.style.maxHeight = "";
+                    height = selectWrapper.offsetHeight;
+                }
+                if(height > options.maxHeight){
+                    height = options.maxHeight + "px";
+                    selectWrapper.style.overflow = "auto";
+                }
+
+                selectWrapper.style.maxHeight = height + "px";
+                selectFullWrapper.classList.add("select-open");
+
+                // Open to top if limited space
+                if(selectHeader.getBoundingClientRect().y + selectHeader.offsetHeight + parseInt(height) > window.innerHeight){
+                    selectWrapper.style.bottom = "100%";
+                    selectWrapper.style.top = "auto";
+                } else {
+                    selectWrapper.style.bottom = "auto";
+                    selectWrapper.style.top = "100%";
+                }
+            } else {
+                selectWrapper.style.maxHeight = "0px";
+                selectFullWrapper.classList.remove("select-open");
+            }
+        });
+
+        window.addEventListener("click", function(e){
+            if(options.dontClose || (options.dontCloseCallback && options.dontCloseCallback() && e.target.classList.contains("custom-select-item")))
+                return;
+
+            if((!e.target || !e.target.parentNode) || (e.target.className != "custom-select-header" && e.target.parentNode.className != "custom-select-header")){
+                selectWrapper.style.maxHeight = "0px";
+                selectFullWrapper.classList.remove("select-open");
+            }
+        });
+
+        // When original select is changed, update new one
+        selectOriginal.addEventListener("change", function(){
+            if(!originalSelectChangedByThis){
+                let selected = this.options[this.selectedIndex].textContent;
+
+                if(selectWrapper.querySelector(".custom-selected").textContent != selected){
+                    [...selectWrapper.children].forEach(function(child){
+                        if(child.textContent == selected){
+                            selectOriginal.nextElementSibling.querySelector("span").textContent = selected;
+                            [...child.parentNode.children].forEach(el => el.classList.remove("custom-selected"));
+                            selectFullWrapper.classList.add("custom-selected");
+                        }
+                    });
+                }
+            }
+
+            originalSelectChangedByThis = false;
+        });
+      
+        // Scrolling on keypress
+        if(options.scrollOnKey){
+            let customSelect = selectOriginal.nextElementSibling;
+
+            customSelect.setAttribute("tabindex", "1");
+            customSelect.addEventListener("keydown", function(e){
+                if(e.keyCode == 27){
+                    // Close on Esc
+                    customSelect.querySelector(".select-items-wrapper").style.maxHeight = "0px";
+                    selectFullWrapper.classList.remove("select-open");
+                }
+
+                let scrolled = false;
+                customSelect.querySelectorAll(".select-items-wrapper div:not(:first)").each(function(el){
+                    if(!scrolled && el.textContent[0].toLowerCase() == e.key){
+                        scrolled = true;
+
+                        // let top = $(this).position().top + $(this).parent().scrollTop();
+                        let top = el.offsetTop + el.parentNode.scrollTop;
+                        customSelect.querySelector(".select-items-wrapper").scrollTo({top: top, behavior: "smooth"});
+                    }
+                });
+            });
+        }
+    });
+}
